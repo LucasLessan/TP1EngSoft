57c3487bc359af38a48a43cfd1f6a5ef3b386f86
djalv <alvaroneto98@protonmail.com>
2024-10-25 22:11:44 -0300

    Frontend da pagina de Login Implementado

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+import LoginPage from './LoginPage'; // Importa o componente de login
+
+function App() {
+  return (
+    <div className="App">
+      <LoginPage /> {/* Renderiza o componente de login */}
+    </div>
+  );
+}
+
+export default App;
