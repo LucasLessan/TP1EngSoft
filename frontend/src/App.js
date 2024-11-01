db90e11c02ddc6c4df32baa1585d1afb91f4cc4e
LucasLessan <lucasteles2495@gmail.com>
2024-11-01 08:24:03 -0300

    Bootstrap instalado e página de login atualizada para usá-lo

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+// src/App.js
+import LoginPage from './components/pages/LoginPage'; // Importa o componente de login
+
+function App() {
+  return (
+    <div className="App">
+      <LoginPage /> {/* Renderiza o componente de login */}
+    </div>
+  );
+};
+
+export default App;
